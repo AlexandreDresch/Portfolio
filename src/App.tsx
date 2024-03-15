9d684d9cf9c1f1d70dd38611d0b95fba13fca094
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-15 00:27:12 -0300

    feat: add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-function App() {
+import ButtonGradient from "./assets/svg/button-gradient"
+import Header from "./components/header"
+
+export default function App() {
 
   return (
     <>
-
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <ButtonGradient />
+    </div>
     </>
   )
 }
-
-export default App
