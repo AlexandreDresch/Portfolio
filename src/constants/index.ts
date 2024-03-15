ed012fcad8275c666604f52f03d8b53d7d8ca66f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-15 00:26:10 -0300

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,34 @@
+export const navigation = [
+    {
+      id: "0",
+      title: "About",
+      url: "#about",
+    },
+    {
+      id: "1",
+      title: "Projects",
+      url: "#projects",
+    },
+    {
+      id: "2",
+      title: "BackEnd",
+      url: "#backend",
+    },
+    {
+      id: "3",
+      title: "FrontEnd",
+      url: "#frontend",
+    },
+    {
+      id: "4",
+      title: "Download CV",
+      url: "#cv",
+      onlyMobile: true,
+    },
+    {
+      id: "5",
+      title: "Contact me",
+      url: "#contact",
+      onlyMobile: true,
+    },
+  ];
