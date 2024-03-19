405ffaa9348b4d28712929232c4a49d2124f2589
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-18 23:04:42 -0300

    feat: add languages icons & add languages to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,22 @@ import search from "../assets/search.svg";
 import plusSquare from "../assets/plus-square.svg";
 import file from "../assets/file.svg";
 
+import angular from "../assets/languages/angularjs-plain.svg";
+import typescript from "../assets/languages/typescript-original.svg";
+import docker from "../assets/languages/docker-plain-wordmark.svg";
+import go from "../assets/languages/go-original-wordmark.svg";
+import javascript from "../assets/languages/javascript-original.svg";
+import nodejs from "../assets/languages/nodejs-original-wordmark.svg";
+import mongodb from "../assets/languages/mongodb-plain-wordmark.svg";
+import reactjs from "../assets/languages/react-original-wordmark.svg";
+import nextjs from "../assets/languages/nextjs-original-wordmark.svg";
+import tailwindcss from "../assets/languages/tailwindcss-original-wordmark.svg";
+import threejs from "../assets/languages/threejs-original-wordmark.svg";
+import springboot from "../assets/languages/spring-original-wordmark.svg";
+import redis from "../assets/languages/redis-plain-wordmark.svg";
+import postgresql from "../assets/languages/postgresql-plain-wordmark.svg";
+import nestjs from "../assets/languages/nestjs-plain-wordmark.svg";
+
 export const navigation = [
   {
     id: "0",
@@ -39,3 +55,21 @@ export const navigation = [
 ];
 
 export const heroIcons = [homeSmile, file, search, plusSquare];
+
+export const languages = [
+  typescript,
+  javascript,
+  go,
+  angular,
+  reactjs,
+  nextjs,
+  nodejs,
+  nestjs,
+  springboot,
+  docker,
+  redis,
+  mongodb,
+  postgresql,
+  tailwindcss,
+  threejs,
+];
