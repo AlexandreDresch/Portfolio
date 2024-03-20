d69fe623e543d60eaf6387758fd162b673d7f6f0
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-19 23:59:18 -0300

    feat: add BackendProjects & About to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import ButtonGradient from "./assets/svg/button-gradient"
-import Header from "./components/header"
-import Hero from "./components/hero"
+import ButtonGradient from "./assets/svg/button-gradient";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import BackendProjects from "./components/backend-projects";
+import About from "./components/about";
 
 export default function App() {
-
   return (
     <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
-      <Hero />
-    </div>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
 
-    <ButtonGradient />
+        <div id="projects">
+          <BackendProjects />
+        </div>
+
+        <About />
+      </div>
+
+      <ButtonGradient />
     </>
-  )
+  );
 }
