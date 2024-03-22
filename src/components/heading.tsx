a5683a351bf55edca0960db7081f901f7b16f34e
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-21 22:13:46 -0300

    feat: add text & tag to heading

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,12 +1,18 @@
+import Tagline from "./tagline";
+
 interface HeadingProps {
-  className: string;
-  title: string;
+  className?: string;
+  title?: string;
+  text?: string;
+  tag?: string;
 }
 
-export default function Heading({ className, title }: HeadingProps) {
+export default function Heading({ className, title, text, tag }: HeadingProps) {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
-      <h2 className="h2 text-center">{title}</h2>
+      {tag && <Tagline className="mb-4 justify-center">{tag}</Tagline>}
+      {title && <h2 className="h2 text-center">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 }
