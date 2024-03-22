aa99730febaf2516f61191ae7cb4e1c983828432
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-21 22:20:07 -0300

    feat: add resume-list component

---
+++ src/components/resume-list.tsx
@@ -0,0 +1,37 @@
+import { resume } from "../constants";
+import Button from "./button";
+
+import check from "../assets/check.svg";
+
+export default function ResumeList() {
+  return (
+    <div className="flex justify-center gap-[1.5rem] max-lg:flex-wrap">
+      {resume.map((item) => (
+        <div
+          key={item.id}
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto py-14 [&>h4]:first:text-color-3 [&>h4]:even:text-color-1 hover:scale-105 transition-transform"
+        >
+          <h4 className="h4 mb-4">{item.title}</h4>
+
+          <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">{item.description}</p>
+
+          <Button white className="w-full mb-6">
+            Download
+          </Button>
+
+          <ul>
+            {
+                item.features.map((feature, index) => (
+                    <li key={index} className="flex items-start py-5 border-t border-n-6">
+                        <img src={check} alt="Check Icon" width={24} height={24} />
+
+                        <p className="body-2 ml-4 text-n-1">{feature}</p>
+                    </li>
+                ))
+            }
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
