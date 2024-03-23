12318b06c628da7eaa314d29df17c5e46b3a212a
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-22 23:51:31 -0300

    feat: add gradient to projects design component

--- src/components/design/projects.tsx
+++ src/components/design/projects.tsx
@@ -1,5 +1,23 @@
-export const GradientLight = () => {
+import gradient from "../../assets/gradient.png";
+
+export function GradientLight() {
   return (
     <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />
   );
-};
+}
+
+export function Gradient() {
+  return (
+    <div className="absolute top-[18.25rem] -left-[30.375rem] w-[56.625rem] opacity-60 mix-blend-color-dodge pointer-events-none">
+      <div className="absolute top-1/2 left-1/2 w-[58.85rem] h-[58.85rem] -translate-x-3/4 -translate-y-1/2">
+        <img
+          className="w-full"
+          src={gradient}
+          width={942}
+          height={942}
+          alt="Gradient"
+        />
+      </div>
+    </div>
+  );
+}
