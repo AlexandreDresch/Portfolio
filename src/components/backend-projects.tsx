d62e1eaedbe248b03bb3998db08f08a906821b1a
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-02 00:01:21 -0300

    feat: getting projects from context & add skeletons

--- src/components/backend-projects.tsx
+++ src/components/backend-projects.tsx
@@ -1,10 +1,15 @@
 import Section from "./section";
 import Heading from "./heading";
-import { projectsBackend } from "../constants";
+import { projectsBackendAssets } from "../constants";
 import Arrow from "../assets/svg/arrow";
 import { GradientLight } from "./design/projects";
+import { useContext } from "react";
+import { ProjectsContext } from "../contexts/projects-context";
+import { Project } from "../constants/types";
 
 export default function BackendProjects() {
+  const { backEndProjects, projectsLoading } = useContext(ProjectsContext);
+
   return (
     <Section
       id="backend"
@@ -20,40 +25,92 @@ export default function BackendProjects() {
         />
       </div>
 
-      <div className="flex justify-center flex-wrap gap-10 px-5 mb-10">
-        {projectsBackend.map((project) => (
-          <div
-            key={project.id}
-            className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] max-w-[24rem]"
-            style={{
-              backgroundImage: `url(${project.backgroundUrl})`,
-            }}
-          >
-            <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem]">
-              <h5 className="h2 text-[1.25rem] md:text-[1.75rem]  mb-5">{project.title}</h5>
-              <p className="body-2 mb-6 text-n-3">{project.text}</p>
+      <div className="flex justify-center flex-wrap gap-6 lg:gap-10 px-5 mb-10">
+        {projectsLoading
+          ? Array.from({ length: 6 }).map((_, index) => {
+              const projectAssets = projectsBackendAssets.find(
+                (asset) => asset.id === index
+              );
 
-              <div className="flex items-center mt-auto">
-                <img
-                  src={project.iconUrl}
-                  width={48}
-                  height={48}
-                  alt={project.title}
-                />
+              const { backgroundUrl } =
+                projectAssets || projectsBackendAssets[0];
 
-                <a href="/" className="ml-auto flex items-center group">
-                  <span className=" font-code text-xs font-bold text-n-1 uppercase tracking-wider group-hover:text-color-6 transition-colors">
-                    See code
-                  </span>
+              return (
+                <div
+                  key={index}
+                  className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] max-w-[24rem]"
+                  style={{
+                    backgroundImage: `url(${backgroundUrl})`,
+                  }}
+                >
+                  <div className="relative z-2 flex flex-col justify-between w-[18rem] sm:min-w-[22rem] h-full min-h-[22rem] p-[2.4rem] animate-pulse">
+                    <div className="h-6 bg-n-1/80 rounded w-3/4 mb-5"></div>
+                    <div className="flex flex-col gap-0">
+                      <div className="h-1 bg-n-1/80 rounded mb-6"></div>
+                      <div className="h-1 bg-n-1/80 rounded mb-6"></div>
+                      <div className="h-1 bg-n-1/80 rounded mb-6"></div>
+                      <div className="h-1 bg-n-1/80 rounded mb-6"></div>
+                      <div className="h-1 bg-n-1/80 rounded mb-6"></div>
+                    </div>
+                    <div className="flex items-center mt-auto">
+                      <div className="bg-n-1/80 rounded w-12 h-12"></div>
+                    </div>
+                  </div>
 
-                  <Arrow />
-                </a>
-              </div>
-            </div>
+                  {index % 2 === 0 && <GradientLight />}
+                </div>
+              );
+            })
+          : backEndProjects.map((project: Project) => {
+              const projectAssets = projectsBackendAssets.find(
+                (asset) => asset.id === project.id
+              );
 
-            {project.light && <GradientLight />}
-          </div>
-        ))}
+              const { backgroundUrl, iconUrl } =
+                projectAssets || projectsBackendAssets[0];
+
+              return (
+                <div
+                  key={project.id}
+                  className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] max-w-[24rem]"
+                  style={{
+                    backgroundImage: `url(${backgroundUrl})`,
+                  }}
+                >
+                  <div className="relative z-2 flex flex-col justify-between h-full min-h-[22rem] p-[2.4rem]">
+                    <h5 className="h2 text-[1.25rem] md:text-[1.75rem]  mb-5">
+                      {project.name}
+                    </h5>
+                    <p className="body-2 mb-6 text-n-3">
+                      {project.description}
+                    </p>
+
+                    <div className="flex items-center mt-auto">
+                      <img
+                        src={iconUrl}
+                        width={48}
+                        height={48}
+                        alt={project.name}
+                      />
+
+                      <a
+                        href={project.github_url}
+                        target="_blank"
+                        className="ml-auto flex items-center group"
+                      >
+                        <span className=" font-code text-xs font-bold text-n-1 uppercase tracking-wider group-hover:text-color-6 transition-colors">
+                          See code
+                        </span>
+
+                        <Arrow />
+                      </a>
+                    </div>
+                  </div>
+
+                  {project.done && <GradientLight />}
+                </div>
+              );
+            })}
       </div>
     </Section>
   );
--- src/components/frontend-projects.tsx
+++ src/components/frontend-projects.tsx
@@ -1,4 +1,3 @@
-import { projectsFrontend } from "../constants";
 import Heading from "./heading";
 import Section from "./section";
 import Tagline from "./tagline";
@@ -7,12 +6,20 @@ import grid from "../assets/grid.png";
 import check from "../assets/check-02.svg";
 import loading from "../assets/loading.svg";
 import { Gradient } from "./design/projects";
+import { useContext } from "react";
+import { ProjectsContext } from "../contexts/projects-context";
+import { Project } from "../constants/types";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
 
 export default function FrontendProjects() {
+  const { width } = useWindowDimensions();
+
+  const { frontEndProjects, projectsLoading } = useContext(ProjectsContext);
+
   return (
     <Section
       id="frontend"
-      className="pt-[12rem] overflow-hidden"
+      className="pt-[8rem] md:pt-[12rem] overflow-hidden"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings={false}
@@ -24,59 +31,119 @@ export default function FrontendProjects() {
           tag="Projects"
         />
 
-        <div className="relative grid gap-6 -ml-2.5 xxs:-ml-0 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
-          {projectsFrontend.map((project) => {
-            const status = project.status === "done" ? "Done" : "In Progress";
-
-            return (
-              <div
-                className={`flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
-                  project.colorful ? "bg-conic-gradient" : "bg-n-6"
-                }`}
-                key={project.id}
-              >
-                <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
-                  <div className="absolute top-0 left-0 max-w-full">
-                    <img
-                      className="w-full"
-                      src={grid}
-                      width={550}
-                      height={550}
-                      alt="Grid"
-                    />
-                  </div>
-                  <div className="relative z-1">
-                    <div className="flex items-center justify-between max-w-[27rem] mb-12 md:mb-20">
-                      <Tagline>{project.date}</Tagline>
-
-                      <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
-                        <img
-                          className="mr-2.5"
-                          src={project.status === "done" ? check : loading}
-                          width={16}
-                          height={16}
-                          alt={status}
-                        />
-                        <div className="tagline text-[0.5rem] md:text-xs">{status}</div>
+        <div className="relative grid gap-6  xxs:-ml-0 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+          {projectsLoading
+            ? Array.from({ length: 6 }).map((_, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="flex p-0.25 rounded-[2.5rem] even:md:translate-y-[7rem] border border-n-6 bg-transparent animate-pulse"
+                  >
+                    <div className="relative  w-full p-8 bg-transparent rounded-[2.4375rem] overflow-hidden xl:p-15">
+                      <div className="absolute top-0 left-0 max-w-full">
+                        <div className="h-20 w-full bg-gray-400 rounded"></div>
+                      </div>
+                      <div className="relative z-1 flex flex-col justify-between h-full">
+                        <div className="flex items-center justify-between w-full md:max-w-[27rem] mb-12 md:mb-20">
+                          <div className="h-4 bg-gray-400 rounded w-1/4"></div>
+                          <div className="flex items-center px-4 py-1 bg-n-1 rounded w-1/4 h-4"></div>
+                        </div>
+                        <div className="mb-10 -my-10 max-h-96 md:h-auto md:-mx-15">
+                          <div className="h-72 w-full bg-gray-400 rounded"></div>
+                        </div>
+                        <div className="h-4 bg-gray-400 rounded w-1/2 mb-2"></div>
+                        <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+                        <div className="flex items-center justify-between mt-3">
+                          <a className="flex items-center group h-5 bg-gray-400 rounded w-1/4"></a>
+                          <a className="flex items-center group h-5 bg-gray-400 rounded w-1/4"></a>
+                        </div>
                       </div>
                     </div>
-
-                    <div className="mb-10 -my-10 md:-mx-15">
-                      <img
-                        className="w-full"
-                        src={project.imageUrl}
-                        width={628}
-                        height={426}
-                        alt={project.title}
-                      />
-                    </div>
-                    <h4 className="h4 mb-4">{project.title}</h4>
-                    <p className="body-2 text-n-4">{project.text}</p>
                   </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              })
+            : frontEndProjects.map((project: Project) => {
+                const status = project.done ? "Done" : "In Progress";
+
+                return (
+                  <div
+                    className={`flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                      project.done ? "bg-conic-gradient" : "bg-n-6"
+                    }`}
+                    key={project.id}
+                  >
+                    <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                      <div className="absolute top-0 left-0 max-w-full">
+                        <img
+                          className="w-full"
+                          src={grid}
+                          width={550}
+                          height={550}
+                          alt="Grid"
+                        />
+                      </div>
+                      <div className="relative z-1 flex flex-col justify-between h-full">
+                        <div className="flex items-center justify-between w-full md:max-w-[27rem] mb-12 md:mb-20">
+                          <Tagline>{project.date}</Tagline>
+
+                          <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
+                            <img
+                              className="mr-2.5"
+                              src={project.done ? check : loading}
+                              width={16}
+                              height={16}
+                              alt={status}
+                            />
+                            <div className="tagline text-[0.5rem] md:text-xs">
+                              {status}
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="mb-10 -my-10 max-h-96 md:h-auto md:-mx-15">
+                          <img
+                            className="w-full h-full object-contain"
+                            src={
+                              width > 768
+                                ? project.images[0]
+                                : project.images[1]
+                            }
+                            width={628}
+                            height={426}
+                            alt={project.name}
+                          />
+                        </div>
+
+                        <h4 className="h4 mb-4">{project.name}</h4>
+                        <p className="body-2 text-n-4">{project.description}</p>
+
+                        <div className="flex items-center justify-between mt-5">
+                          {project.deployment_url && (
+                            <a
+                              href={project.deployment_url}
+                              target="_blank"
+                              className="flex items-center group"
+                            >
+                              <span className="font-code text-sm md:text-base font-bold text-n-1 uppercase tracking-wider group-hover:text-color-6 transition-colors">
+                                Visit Demo
+                              </span>
+                            </a>
+                          )}
+                          <a
+                            href={project.github_url}
+                            target="_blank"
+                            className="flex items-center group"
+                          >
+                            <span className="font-code text-sm md:text-base font-bold text-n-1 uppercase tracking-wider group-hover:text-color-6 transition-colors">
+                              See Code
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
 
           <Gradient />
         </div>
